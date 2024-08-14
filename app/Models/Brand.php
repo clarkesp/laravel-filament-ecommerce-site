8e384b84d1d987b2052404bef4054f767bc9da85
clarkesp <clarke.noddie.io@gmail.com>
2024-08-14 17:55:30 -0300

    fillables

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug', 'image', 'is_active'];
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'grand_total', 'payment_method', 'status', 'currency', 'delivery_amount', 'delivery_method', 'note'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function items() {
+        return $this->hasMany(OrderItem::class);
+    }
+    public function address() {
+        return $this->hasOne(Address::class);
+    }
 }
