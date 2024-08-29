c8a8367f56c07914e9d1920fc80109a3d9dfe492
clarkesp <clarke.noddie.io@gmail.com>
2024-08-29 20:53:54 -0300

    Livewire Pages home and categories

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\CategoriesPage;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\HomePage;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
