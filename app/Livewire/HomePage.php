fdbeb2d24dc888081d594005b719353f48c6c869
clarkesp <clarke.noddie.io@gmail.com>
2024-08-30 07:38:03 -0300

    Livewire Fucntions - dynamic feed for home page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Store\Brand;
+use Livewire\Attributes\Title;
 use Livewire\Component;
-
+#[Title('Home Page - Ecommerce')]
 class HomePage extends Component
 {
     public function render()
     {
+        $brands = Brand::where('is_active', 1)->get();
+        dd($brands);
         return view('livewire.home-page');
     }
 }
